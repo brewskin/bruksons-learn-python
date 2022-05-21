d37354deb61898aef7ac0257327762c12d65eaa4
Olya <ribak.olya19@gmail.com>
2022-05-21 16:37:17 -0400

    playing  with print

--- examples/printing.py
+++ examples/printing.py
@@ -1,33 +1,38 @@
-# print on a new line directly
-print("hey this is a string being printed!")
+# # print on a new line directly
+# print("Canada is not so bad")
 
-# declare my variable
-x = "my variable is printing!"
-# print my variable
-print(x)
+# # declare my variable
+# x = "a string saved into a variable"
+# # print my variable
+# print(x)
 
-# print an empty line
-print()
+# # print an empty line
+# print()
 
-# declare my variables
-myName = "Sasha"
-myAge = 32
-# print a sentence
-print("Hello", myName, "you are", myAge, "years old")
-# change the seperator
-print("Hello", myName, "you are", myAge, "years old", sep="--")
-# remove seperator
-print("Hello", myName, "you are", myAge, "years old", sep="")
+# # declare my variables
+# myName = "Olya"
+# myAge = 26
+# # print a sentence
+# print("Hello", myName, "you are", myAge, "years old")
+# # change the seperator
+# print("Hello", myName, "you are", myAge, "years old", sep="--")
+# # remove seperator
+# print("Hello", myName, "you are", myAge, "years old", sep="")
 
 # empty line
+# print()
+
+# # declare array variable
+# words = ["dont", "ask", "too", "many", "text message"]
+# # print my variables
+# # print(words[1])
+
+# # printing the array
+# print("The sentence is: ")
+# for element in words:
+#     print(element, end=' ')
+
 print()
+myname="olya"
+print(myname, "is", "the", "best", sep="*****",end="with\n")
 
-# declare array variable
-words = ["dont", "ask", "too", "many", "questions"]
-# print my variables
-print(words)
-
-# printing the array
-print("The sentence is: ")
-for i in words:
-    print(i, end=' ')
